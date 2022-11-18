9032199bcb6a593759a07577e6f1ce7a432b0711
Rosario Polito <rosario.polito@gmx.de>
2022-11-18 18:35:56 +0100

    adding ticket to system

--- htdocs/setup.sql
+++ htdocs/setup.sql
@@ -19,3 +19,30 @@ insert into categorie(cat_name) values("Hardware");
 insert into categorie(cat_name) values("Software");
 
 select * from categorie;
+
+/* Ticket */
+
+CREATE TABLE ticket (
+    ticket_id INT AUTO_INCREMENT,
+    titel VARCHAR(16),
+    frage VARCHAR(255),
+    antwort VARCHAR(255),
+    ticket_status VARCHAR(2),
+    
+    categorie VARCHAR(16),
+    fragensteller VARCHAR(16),
+
+    PRIMARY KEY (ticket_id),
+    FOREIGN KEY(categorie) REFERENCES categorie(cat_name),
+    FOREIGN KEY(fragensteller) REFERENCES user(username)
+);
+/*
+    LEGENDE Status:
+    QA - QUESTION ASKED
+    NA - NEEDS APPROVAL
+    OK - Fertig
+*/
+
+insert into ticket (titel, frage, antwort, ticket_status, categorie, fragensteller)
+values("Titel Frage 1", "Eine etwas zu kurz geratene Frage", 
+"Eine etwas zu kurz geratene Anrtwort", "NA", "Software", "admin");
